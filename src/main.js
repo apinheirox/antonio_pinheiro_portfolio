b43bde0f6a436e16098e51f751b0c881a2f22525
Antônio Pinheiro <techwoodev@gmail.com>
2024-04-25 18:00:05 -0300

    Ajustando os ultimos detalhes

--- src/main.js
+++ src/main.js
@@ -21,21 +21,30 @@ menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('active');
     navMenu.classList.toggle('active');
 });
+window.addEventListener("load", function() {
+    setTimeout(function() {
+        var loader = document.getElementById("loader");
+        loader.style.opacity = 0;
+        loader.style.pointerEvents = "none";
 
-
+        var content = document.getElementById("content");
+        content.style.opacity = 1;
+        content.style.pointerEvents = "auto";
+    }, 5000);
+});
 function enviarParaWhatsApp() {
     var nome = document.getElementById("nome").value;
     var telefone = document.getElementById("telefone").value;
     var email = document.getElementById("email").value;
     var mensagem = document.getElementById("mensagem").value;
 
-    // Verificar se todos os campos estão preenchidos
+    
     if (nome === "" || telefone === "" || email === "" || mensagem === "") {
         alert("Por favor, preencha todos os campos do formulário antes de enviar sua mensagem.");
-        return; // Impede o envio se algum campo estiver vazio
+        return; 
     }
 
-    // Mensagem formatada para o WhatsApp
+    
     var mensagemWhatsApp =
         "Mensagem do Cliente:\n\n" +
         "Nome: " + nome + "\n" +
@@ -43,13 +52,13 @@ function enviarParaWhatsApp() {
         "E-mail: " + email + "\n\n" +
         "Mensagem:\n" + mensagem;
 
-    // Substitua o número de telefone abaixo pelo número desejado com o prefixo internacional (ex: 5511999999999)
+    
     var numeroWhatsApp = "5571984652983";
 
-    // URL para a API do WhatsApp
+    
     var url = "https://wa.me/" + numeroWhatsApp + "?text=" + encodeURIComponent(mensagemWhatsApp);
 
-    // Abrir a URL no WhatsApp
+    
     window.open(url);
 }
 
